cf1e8a36dcef28c03c20dad0cd0456156c190880
claudiocassimiro <days_surfing@live.com>
2023-07-01 15:11:42 -0300

    feat: implements controller to handle product creation

---
+++ src/controllers/createProductController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { createProductService } from '../services/createProductsService';
+import type { Products } from '@prisma/client';
+import { productValidation } from '../helpers/productValidation';
+
+export const createProductController = async (req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body as Products;
+    productValidation({ name, description, price });
+
+    const product = await createProductService({
+      name,
+      description,
+      price,
+    });
+
+    if (!product) throw Error;
+
+    return res.status(201).json({
+      message: 'Novo produto criado com sucesso',
+      productId: product.id,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Erro ao salvar o produto no banco',
+      error: err.message,
+    });
+  }
+};
