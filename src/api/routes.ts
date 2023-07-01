1237d19e3de25327e2f406de5e47febb7b8ac146
claudiocassimiro <days_surfing@live.com>
2023-07-01 15:55:00 -0300

    feat: implements router files

---
+++ src/api/routes.ts
@@ -0,0 +1,13 @@
+import { createProductController } from '../controllers/createProductController';
+import { getProductsController } from '../controllers/getProductsController';
+import { deleteProductController } from '../controllers/deleteProductController';
+import { getProductByIdController } from '../controllers/getProductByIdController';
+import { editProductController } from '../controllers/editProductController';
+
+export {
+  createProductController,
+  getProductsController,
+  deleteProductController,
+  getProductByIdController,
+  editProductController,
+};
