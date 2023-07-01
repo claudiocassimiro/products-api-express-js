8cf17170fbbb09a9d2f737cc44f9184f3112c4eb
claudiocassimiro <days_surfing@live.com>
2023-07-01 15:38:33 -0300

    fix: hotfix

--- src/services/createProductsService.ts
+++ src/services/createProductsService.ts
@@ -1,6 +1,11 @@
 import prisma from '../lib/prisma';
+import { Product } from '../helpers/types';
 
-export const createProductService = async ({ name, description, price }) => {
+export const createProductService = async ({
+  name,
+  description,
+  price,
+}: Product) => {
   try {
     const product = await prisma.products.create({
       data: { name, description, price },
