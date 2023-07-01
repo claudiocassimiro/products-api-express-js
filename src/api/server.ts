af273749502cc4726667eb32c76b7e6c2ff5cad1
claudiocassimiro <days_surfing@live.com>
2023-07-01 15:11:15 -0300

    fix: put server.ts inside api folder

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { createProductController } from './routes';
 
 dotenv.config();
 
 const app = express();
+const apiRoutes = express.Router();
+
+app.use(express.json());
+app.use(apiRoutes);
+
+apiRoutes.post('/api/createProduct', createProductController);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App running on port ${process.env.PORT || 3000}`);
 });
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello, World!' });
+  return res.send({ message: 'Hello, world!' });
 });
