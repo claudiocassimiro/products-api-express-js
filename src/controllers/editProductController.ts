93c447cf5cecb555c5345b3d95bf9572ed3b054b
claudiocassimiro <days_surfing@live.com>
2023-07-02 11:53:07 -0300

    fix: hotfix

--- src/controllers/editProductController.ts
+++ src/controllers/editProductController.ts
@@ -5,12 +5,16 @@ import type { Products } from '@prisma/client';
 export const editProductController = async (req: Request, res: Response) => {
   const productData = req.body as Products;
 
+  const { id } = productData;
+
+  if (!id) return res.status(422).json({ message: 'Informe o id do produto' });
+
   try {
     const product = await editProductService(productData);
 
     if (!product) throw new Error();
 
-    return res.status(201).json({
+    return res.status(200).json({
       message: 'Produto editado com sucesso',
       product,
     });
