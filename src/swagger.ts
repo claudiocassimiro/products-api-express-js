512a7d8de33ac70f68e9edbe29bf0b1df0261574
claudiocassimiro <days_surfing@live.com>
2023-07-02 22:30:07 -0300

    fix: fix paths to work in localhost and production mode

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,6 +2,17 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import * as swaggerUI from 'swagger-ui-express';
 import { Express } from 'express';
 
+import path from 'path';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const basePath = isProduction ? 'dist/api' : 'src/api';
+
+const apiFiles = [
+  path.join(basePath, '**/*.ts'),
+  path.join(basePath, '**/*.js'),
+];
+
 const options: swaggerJSDoc.Options = {
   swaggerDefinition: {
     info: {
@@ -9,13 +20,13 @@ const options: swaggerJSDoc.Options = {
       version: '1.0.0',
     },
   },
-  apis: ['src/api/*.ts'],
+  apis: apiFiles,
 };
 
 const setupSwagger = (app: Express) => {
   const swaggerSpec = swaggerJSDoc(options);
 
-  app.get('/api-docs.json', (req, res) => {
+  app.get('/api-docs ', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
